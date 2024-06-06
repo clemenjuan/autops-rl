05e6ee81adbcdb54e421e240c105ed1325a4286c
Clemente Mac <ge26sav@tum.de>
2024-06-06 15:28:59 +0200

    Update training.py to adjust resource allocation for training

--- training.py
+++ training.py
@@ -21,10 +21,10 @@ import pandas as pd
 # Common configuration setup
 # Resource allocation settings
 def setup_config(config):
-    num_rollout_workers = 11 # Number of rollout workers (parallel actors for simulating environment interactions)
+    num_rollout_workers = 10 # Number of rollout workers (parallel actors for simulating environment interactions)
     num_envs_per_worker = 1 # Number of environments per worker
     num_cpus_per_worker = 1 # Number of CPUs per worker
-    num_cpus_per_learner_worker = 11 # Number of CPUs per local worker (trainer)
+    num_cpus_per_learner_worker = 10 # Number of CPUs per local worker (trainer)
 
     config.environment(env=env_name, env_config=env_config, disable_env_checking=True)
     config.framework(args.framework)
